310122ee98985a081abbb180a1ed3aef09041bbf
Isabelle <isabel.tartarelli@live.it>
2023-05-31 20:55:53 +0100

    Added description and created 2 more files

--- Main/state.js
+++ Main/state.js
@@ -15,46 +15,64 @@ const state = {
 			id: '002-Wolf-Jacket',
 			name: 'Wolf Jacket',
 			price: 38.25,
+			description:
+				'Rimani al riparo dalle intemperie grazie a questa giacca Champion ripiegabile. Questa giacca in poliestere resistente alla pioggia e al vento arricchita da un prezioso ricamo ha un pratico cappuccio, un tascone frontale e una tasca con cerniera estraibile nella quale può essere riposta.',
 		},
 		{
 			id: '003-Wolf-Hat',
 			name: 'Wolf Hat',
 			price: 23.0,
+			description:
+				'Questo cappellino ha una vestibilità classica, una tesa piatta ed è interamente realizzato in tela rigida. Inoltre, il suo sistema di chiusura a scatto regolabile lo rende estremamente comodo da indossare.',
 		},
 		{
 			id: '004-Wolf-Hoodie',
 			name: 'Wolf Hoodie',
 			price: 30.0,
+			description:
+				"Tutti hanno bisogno di un'avvolgente felpa con cappuccio infallibile per raggomitolarcisi dentro, per questo scegline una morbida, liscia e di gran stile. È la scelta perfetta per le serate più fredde!",
 		},
 		{
 			id: '005-Wolf-Man-T-Shirt',
 			name: 'Wolf Man T-Shirt',
 			price: 15.0,
+			description:
+				'Hai trovato la maglietta essenziale del tuo guardaroba. È realizzata in 100% cotone filato ad anello ed è morbida e comoda. La doppia cucitura sulla scollatura e sulle maniche donano una maggiore durata a quella che diventerà sicuramente la tua maglietta preferita!',
 		},
 		{
 			id: '006-Wolf-Woman-White-T-Shirt',
 			name: 'Wolf Woman White T-Shirt',
 			price: 15.0,
+			description:
+				'La tipica t-shirt in cotone al 100% (eccetto per le tinte mélange che contengono il 10% di poliestere). Il tessuto viene prelavato per garantire il mantenimento delle sue dimensioni anche dopo diversi lavaggi e la t-shirt ha una vestibilità classica.',
 		},
 		{
 			id: '007-Wolf-Woman-Black-t-Shirt',
 			name: 'Wolf Hoodie',
 			price: 15.0,
+			description:
+				'La tipica t-shirt in cotone al 100% (eccetto per le tinte mélange che contengono il 10% di poliestere). Il tessuto viene prelavato per garantire il mantenimento delle sue dimensioni anche dopo diversi lavaggi e la t-shirt ha una vestibilità classica.',
 		},
 		{
 			id: '008-Wolf-Zipped-Hoodie',
 			name: 'Wolf Zipped Hoodie',
 			price: 36.0,
+			description:
+				"Quando la temperatura scende nelle sere d'estate in riva a un lago o quando semplicemente vuoi indossare qualcosa di comodo, questa leggera felpa unisex dal taglio moderno con cappuccio, zip sul davanti e una tasca a marsupio è la soluzione che fa al caso tuo.",
 		},
 		{
 			id: '009-Wolf-White-Jersey',
 			name: 'Wolf Jersey',
 			price: 20.5,
+			description:
+				'Un classico senza tempo pensato per chiunque sia alla ricerca di eccellente qualità e morbidezza.',
 		},
 		{
 			id: '010-Wolf-Bottle',
 			name: 'Wolf Hoodie',
 			price: 28.0,
+			description:
+				'Questa borraccia in acciaio inox da mezzo litro e con struttura a doppia parete sarà perfetta per le tue uscite quotidiane, mantenendo la tua bevanda preferita alla temperatura ideale per ore! Avendo un tappo inodore ed ermetico, potrai lanciarla in macchina mentre vai al lavoro, portarla con te durante le escursioni, oppure metterla in borsa e lasciarla muovere liberamente.',
 		},
 	],
 	cart: [],
