f5ed876d915e818f52ea402d8827bb8cfc65e302
Isabelle <isabel.tartarelli@live.it>
2023-06-01 17:55:53 +0100

    Started Cart - need to do better css for second page

--- ItemPages/common.js
+++ ItemPages/common.js
@@ -26,11 +26,44 @@ function load() {
 	// Item will be the -item- I clicked on from the last page.
 }
 
+// function addItemToCart(item) {
+// 	if (!isItemInCart(item.name)) {
+// 		addItemToCart(item)
+// 	}
+// 	state.cart.push(item)
+
+// 	updateQuantity(item)
+// }
+
+// function updateQuantity(quantityItem) {
+// 	const quantityItems = state.cart.filter(
+// 		(cartItem) => cartItem.name === quantityItem.name
+// 	)
+// 	const numOfItems = quantityItems.length
+// 	const escapedName = CSS.escape(quantityItem.name)
+// 	const element = document.querySelector(`#${escapedName} span.quantity-text`)
+// 	if (element) {
+// 		element.innerText = numOfItems.toString()
+// 	}
+// 	itemsTotalPrice()
+// }
+
+// function isItemInCart(itemName) {
+// 	const itemsInCart = state.cart.filter(
+// 		(cartItem) => cartItem.name === itemName
+// 	)
+// 	if (itemsInCart.length > 0) {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
 function displayProduct(item) {
 	const body = document.querySelector('body')
 
-  const div = document.createElement('div')
-  div.className = 'container'
+	const div = document.createElement('div')
+	div.className = 'container'
 	body.append(div)
 
 	const h1 = document.createElement('h1')
@@ -60,25 +93,14 @@ function displayProduct(item) {
 	description.className = 'description'
 	div.append(description)
 
-
 	const feature = displayProductFeatures(item)
 	feature.className = 'feature'
 	div.append(feature)
 
-	const addToCart = document.createElement('button')
-	addToCart.className = 'add-to-cart'
-	addToCart.innerText = 'Add To Cart'
-	div.append(addToCart)
-
 	const buyNow = document.createElement('button')
 	buyNow.className = 'buy-now'
 	buyNow.innerText = 'Buy Now'
-  div.append(buyNow)
-  
-//   const cart = document.createElement('img')
-//   cart.src = '../img/cart.svg'
-// cart.className = 'cart-img'
-//   h1.append(cart)
+	div.append(buyNow)
 
 	const footer = document.createElement('footer')
 	footer.className = 'footer'
@@ -86,17 +108,18 @@ function displayProduct(item) {
 
 	const link = document.createElement('a')
 	link.href = 'https://streamlabs.com/it-it/creator-sites'
-  footer.append(link)
-  
+	footer.append(link)
+
 	const logo = document.createElement('img')
 	logo.src = '/img/Streamlabs-Logo.png'
-  link.append(logo)
-  
-  const terms = document.createElement('p')
-  terms.className = 'terms'
-  terms.innerText = '© 2023 General Workings Inc. Interagendo con questa pagina accetti i nostri <b>Termini &\
+	link.append(logo)
+
+	const terms = document.createElement('p')
+	terms.className = 'terms'
+	terms.innerText =
+		'© 2023 General Workings Inc. Interagendo con questa pagina accetti i nostri <b>Termini &\
         condizioni and Cookie Policy.'
-  body.append(terms)
+	body.append(terms)
 }
 
 function addImage(item) {
@@ -119,7 +142,32 @@ function displayProductFeatures(item) {
 
 function displayFooter() {
 	const footer = document.createElement('footer')
-
 	return footer
 }
+// const addToCart = document.createElement('button')
+// addToCart.className = 'add-to-cart'
+// addToCart.innerText = 'Add To Cart'
+// addToCart.addEventListener('click', () => {
+// 	addItemToCart(item)
+// })
+// div.append(addToCart)
+// function addQuantity(item) {
+// 	const quantityButton = document.createElement('button')
+// 	quantityButton.id = item.name
+// 	addToCart.append(quantityButton)
+// }
+
+// function itemsTotalPrice() {
+// 	const totalPrice = document.querySelector('.total-number')
+// 	let total = 0
+// 	state.cart.forEach((item) => {
+// 		const price = item.price
+// 		total += price
+// 	})
+// 	if (total == 0) {
+// 		totalPrice.innerText = '£ 0.00'
+// 	} else {
+// 		totalPrice.innerText = `£ ${total.toFixed(2).toString()}`
+// 	}
+// }
 load()
