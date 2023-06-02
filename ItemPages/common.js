58e0e04d4d50afb13af6310f8fbf91dd85ef649e
Isabelle <isabel.tartarelli@live.it>
2023-06-02 12:00:57 +0100

    Made feature into list and started js for add to cart

--- ItemPages/common.js
+++ ItemPages/common.js
@@ -26,87 +26,108 @@ function load() {
 	// Item will be the -item- I clicked on from the last page.
 }
 
-// function addItemToCart(item) {
-// 	if (!isItemInCart(item.name)) {
-// 		addItemToCart(item)
-// 	}
-// 	state.cart.push(item)
+function addItemToCart(item) {
+	if (!isItemInCart(item.name)) {
+		addCounter(item)
+	}
+	state.cart.push(item)
 
-// 	updateQuantity(item)
-// }
+	updateQuantity(item)
+}
 
 // function updateQuantity(quantityItem) {
 // 	const quantityItems = state.cart.filter(
 // 		(cartItem) => cartItem.name === quantityItem.name
 // 	)
 // 	const numOfItems = quantityItems.length
-// 	const escapedName = CSS.escape(quantityItem.name)
-// 	const element = document.querySelector(`#${escapedName} span.quantity-text`)
-// 	if (element) {
-// 		element.innerText = numOfItems.toString()
-// 	}
-// 	itemsTotalPrice()
+
+// 	// itemsTotalPrice()
 // }
 
-// function isItemInCart(itemName) {
-// 	const itemsInCart = state.cart.filter(
-// 		(cartItem) => cartItem.name === itemName
-// 	)
-// 	if (itemsInCart.length > 0) {
-// 		return true
-// 	} else {
-// 		return false
-// 	}
-// }
+function isItemInCart(itemName) {
+	const itemsInCart = state.cart.filter(
+		(cartItem) => cartItem.name === itemName
+	)
+	if (itemsInCart.length > 0) {
+		return true
+	} else {
+		return false
+	}
+}
+const body = document.querySelector('body')
 
+/* CONTAINER */
 function displayProduct(item) {
-	const body = document.querySelector('body')
-
 	const div = document.createElement('div')
 	div.className = 'container'
 	body.append(div)
 
+	/* H1 */
 	const h1 = document.createElement('h1')
 	h1.innerText = 'Wolframe96'
 	div.append(h1)
 
+	/* IMG */
 	const img = addImage(item)
 	img.className = 'product-img'
 	div.append(img)
 
+	/* NAME */
 	const prodName = document.createElement('h2')
 	prodName.innerText = item.name
 	prodName.className = 'product-name'
 	div.append(prodName)
 
+	/* PRICE */
 	const priceText = document.createElement('p')
 	priceText.className = 'price-text'
 	priceText.innerText = 'Price'
 	div.append(priceText)
 
+	/* ACTUAL PRICE */
 	const price = document.createElement('p')
 	price.className = 'price-tag'
 	price.innerText = '$' + item.price
 	div.append(price)
 
+	/* DESCRIPTION */
 	const description = displayProductDescription(item)
 	description.className = 'description'
 	div.append(description)
 
-	const feature = displayProductFeatures(item)
-	feature.className = 'feature'
-	div.append(feature)
+	/* FEATURE */
+	// const feature = displayProductFeatures(item)
+	// feature.className = 'feature'
+	// div.append(feature)
+	const features = item.feature
+	console.log('ft', features)
+	const featureList = document.createElement('ul')
+	featureList.className = 'feature'
+  div.append(featureList)
+  
+	features.forEach((item) => {
+		const feature = document.createElement('li')
+		feature.innerText = item
 
+		featureList.append(feature)
+	})
+
+	/* ADD TO CART BUTTON */
 	const addToCart = document.createElement('button')
 	addToCart.className = 'add-to-cart'
 	addToCart.innerText = 'Add To Cart'
+	addToCart.addEventListener('click', () => {
+		addItemToCart(item)
+	})
 	div.append(addToCart)
 
+	/* BUY NOW BUTTON */
 	const buyNow = document.createElement('button')
 	buyNow.className = 'buy-now'
 	buyNow.innerText = 'Buy Now'
 	div.append(buyNow)
 
+	/* FOOTER */
 	const footer = document.createElement('footer')
 	footer.className = 'footer'
 	body.append(footer)
@@ -127,6 +148,13 @@ function displayProduct(item) {
 	body.append(terms)
 }
 
+function addCounter(item) {
+	const counter = document.createElement('div')
+	counter.id = item.name
+	counter.className = 'counter'
+	counter.innerText = '1'
+	body.append(counter)
+}
 function addImage(item) {
 	const image = document.createElement('img')
 	image.src = `../img/Prodotti/${item.id}.png`
@@ -149,13 +177,7 @@ function displayFooter() {
 	const footer = document.createElement('footer')
 	return footer
 }
-// const addToCart = document.createElement('button')
-// addToCart.className = 'add-to-cart'
-// addToCart.innerText = 'Add To Cart'
-// addToCart.addEventListener('click', () => {
-// 	addItemToCart(item)
-// })
-// div.append(addToCart)
+
 // function addQuantity(item) {
 // 	const quantityButton = document.createElement('button')
 // 	quantityButton.id = item.name
